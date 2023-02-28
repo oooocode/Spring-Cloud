83916576d4be764bc363efc9da9c6d9d3d379274
Wth <29977@qq.com>
2023-02-28 15:26:30 +0800

    nacos的学习

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: wth
+ * @Create: 2023/2/26 - 15:36
+ */
+@RestController
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+/**
+ * @Author: wth
+ * @Create: ${DATE} - ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
